fce848b3274e2e99ad75bff8962a126aa1b95d8f
TheGreatestRock <72756140+TheGreatestRock@users.noreply.github.com>
2024-01-21 12:44:10 +0100

    re petit fix

--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify';
+import vuetify from './plugins/vuetify';
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
-
-Vue.use(Vuetify);
-
-const vuetify = new Vuetify({
-  theme: {
-    themes: {
-      light: {
-        primary: '#3f51b5',
-        secondary: '#b0bec5',
-        accent: '#8c9eff',
-        error: '#b71c1c'
-      }
-    }
-  }
-});
 
 new Vue({
   vuetify,
