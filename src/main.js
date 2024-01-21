2ad5cf2431ef0af222e3ac8659292c5ce3b21c27
TheGreatestRock <72756140+TheGreatestRock@users.noreply.github.com>
2024-01-21 12:42:20 +0100

    petit fix pour vuetify et ajout d'une navbar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
 import Vue from 'vue'
+import Vuetify from 'vuetify';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#3f51b5',
+        secondary: '#b0bec5',
+        accent: '#8c9eff',
+        error: '#b71c1c'
+      }
+    }
+  }
+});
+
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
--- src/store/modules/heroes.js
+++ src/store/modules/heroes.js
@@ -17,5 +17,4 @@ export default {
         }
     },
     actions: {},
-    getters: {}
 }
