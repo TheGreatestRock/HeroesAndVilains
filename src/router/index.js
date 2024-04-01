b5936baabd89c23c6661a6674e52dab1bc23ce15
TheGreatestRock <72756140+TheGreatestRock@users.noreply.github.com>
2024-04-01 15:16:51 +0200

    AHAHAHAHA

--- src/router/index.js
+++ src/router/index.js
@@ -1,72 +1,88 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+//import store from '../store';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        // component: HomeView
-    },
-    {
-        path: '/authentication',
-        name: 'authentication',
-        component : () => import('../views/AuthenticationView.vue'),
-        children: []
-    },
-    {
-        path: '/organisations',
-        name: 'organisations',
-        component: () => import('../views/OrganisationsView.vue'),
-        children: [
-            {
-                path: '/organisations/',
-                name: 'organisationsList',
-                components: {
-                    organisationMain: () => import('../components/OrganisationsList.vue')
-                }
-            },
-            {
-                path: '/organisations/current',
-                name: 'currentOrganisationDetails',
-                components: {
-                    organisationMain: () => import('../components/OrganisationDetails.vue')
-                }
-            }
-        ]
-    },
-    {
-        path: '/teams',
-        name: 'teams',
-        component: () => import('../views/TeamsView.vue'),
-        children: [
-            {
-                path: '/teams/',
-                name: 'teamsList',
-                components: {
-                    teamMain: () => import('../components/TeamsList.vue')
-                }
-            },
-            {
-                path: '/teams/current',
-                name: 'currentTeamDetails',
-                components: {
-                    teamMain: () => import('../components/TeamDetails.vue')
-                }
-            }
-        ]
-    },
-    {
-        path: '/about',
-        name: 'about',
-    }
-]
+  {
+    path: '/',
+    name: 'home',
+    meta: { requiresAuth: false },
+  },
+  {
+    path: '/authentication',
+    name: 'authentication',
+    component: () => import('../views/AuthenticationView.vue'),
+    children: [],
+    meta: { requiresAuth: false },
+  },
+  {
+    path: '/organisations',
+    name: 'organisations',
+    component: () => import('../views/OrganisationsView.vue'),
+    children: [
+      {
+        path: '/organisations/',
+        name: 'organisationsList',
+        components: {
+          organisationMain: () => import('../components/OrganisationsList.vue'),
+        },
+        meta: { requiresAuth: false },
+      },
+      {
+        path: '/organisations/current',
+        name: 'currentOrganisationDetails',
+        components: {
+          organisationMain: () => import('../components/OrganisationDetails.vue'),
+        },
+        meta: { requiresAuth: true },
+      },
+    ],
+  },
+  {
+    path: '/teams',
+    name: 'teams',
+    component: () => import('../views/TeamsView.vue'),
+    children: [
+      {
+        path: '/teams/',
+        name: 'teamsList',
+        components: {
+          teamMain: () => import('../components/TeamsList.vue'),
+        },
+        meta: { requiresAuth: true },
+      },
+      {
+        path: '/teams/current',
+        name: 'currentTeamDetails',
+        components: {
+          teamMain: () => import('../components/TeamDetails.vue'),
+        },
+        meta: { requiresAuth: true },
+      },
+    ],
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/about',
+    name: 'about',
+  },
+  {
+    path: '*',
+    redirect: '/',
+  },
+  
+];
+
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
-})
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
 
-export default router
+
+
+
+export default router;
