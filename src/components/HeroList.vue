f750f15ac42589c9b9be2f67d2ab3ff6c27101b3
TheGreatestRock <72756140+TheGreatestRock@users.noreply.github.com>
2024-04-07 15:26:49 +0200

    changes

--- src/components/HeroList.vue
+++ src/components/HeroList.vue
@@ -1,34 +1,74 @@
 <template>
   <div class="ma-10">
+
+
     <h2>List of all heroes</h2>
+
+    <!-- Buttons -->
     <v-btn @click="showCreateDialog = true;" color="primary" class="mr-5">
-      <span v-if="!heroList">Create new hero</span>
-      <span v-else>Create and add new hero</span>
+      <span v-if="!heroList">
+        Create new hero
+      </span>
+      <span v-else>
+        Create and add new hero
+      </span>
     </v-btn>
-    <v-btn v-if="heroList && addableHero.length > 0" @click="showAdditionDialog = true" color="primary" class="mr-5">Add hero</v-btn>
-    <v-btn @click="select" color="primary">View</v-btn>
+    <v-btn v-if="heroList" @click="showAdditionDialog = true" color="primary" class="mr-5">Add hero</v-btn>
+    <v-btn @click="selectHero" color="primary">View</v-btn>
+
+    <!-- List -->
     <v-card class="mx-auto" max-width="700">
-      <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details>
+      <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Chercher"
+          single-line
+          hide-details
+      >
         <template v-slot:append-outer>
           <v-icon @click="search=''">mdi-close</v-icon>
         </template>
       </v-text-field>
-      <v-data-table v-model="selectedHero" :headers="headers" :items="members" item-key="_id" show-select :single-select="true" class="elevation-1">
+      <v-data-table
+          v-model="selected"
+          :headers="headers"
+          :items="members"
+          :single-select="true"
+          :search="search"
+          item-key="_id"
+          show-select
+          class="elevation-1"
+      >
         <template slot="item.actions" slot-scope="{ item }">
-          <v-icon small @click="deletionAction(item)">mdi-delete</v-icon>
+          <v-icon
+              small
+              @click="delectionAction(item)"
+          >
+            mdi-delete
+          </v-icon>
         </template>
       </v-data-table>
     </v-card>
+
+    {{ addableHero }}
+
+    <!-- Dialog to create new hero -->
     <v-dialog persistent v-model="showCreateDialog">
       <v-card>
         <v-card-title class="text-h5">
-          <span v-if="!heroList">Create new Hero</span>
-          <span v-else>Create and add new Hero</span>
+          <span v-if="!heroList">
+            Create new Hero
+          </span>
+          <span v-else>
+            Create and add new Hero
+          </span>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
-              <v-alert v-model="showCreationError" color="red" type="error">Creation failed</v-alert>
+              <v-alert v-model="showCreationError" color="red" type="error">
+                Creation failed
+              </v-alert>
             </v-row>
             <v-row>
               <v-text-field label="Hero's real name" v-model="heroCreation.realName"/>
@@ -40,8 +80,16 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer/>
-          <v-btn color="red darken-1" text @click="showCreateDialog = false; showCreationError = false">Cancel</v-btn>
-          <v-btn color="blue darken-1" text @click="confirmCreate">Confirm</v-btn>
+          <v-btn
+              color="red darken-1"
+              text
+              @click="showCreateDialog = false; showCreationError = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="confirmCreate">
+            Confirm
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -49,9 +97,17 @@
     <!-- Dialog to add an existing team to the organisation -->
     <v-dialog persistent v-model="showAdditionDialog">
       <v-card>
-        <v-card-title>Addition of team to organisation {{ getCurrentOrganisation.name || "" }}</v-card-title>
+        <v-card-title>
+          Addition of team to organisation {{ getCurrentOrganisation.name || "" }}
+        </v-card-title>
         <v-card-text>
-          <v-combobox v-model="heroesToAdd" multiple :items="addableHero" item-text="publicName" label="Select hero"></v-combobox>
+          <v-combobox
+              v-model="heroesToAdd"
+              multiple
+              :items="addableHero"
+              item-text="publicName"
+              label="Select hero"
+          ></v-combobox>
         </v-card-text>
         <v-card-actions>
           <v-btn
@@ -85,8 +141,16 @@
           Are you sure you want to delete this hero from your team ?
         </v-card-text>
         <v-card-actions>
-          <v-btn color="red darken-1" text @click="showDeletionDialog = false; showDeletionError = false">Cancel</v-btn>
-          <v-btn color="blue darken-1" text @click="confirmDeletion">Confirm</v-btn>
+          <v-btn
+              color="red darken-1"
+              text
+              @click="showDeletionDialog = false; showDeletionError = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="confirmDeletion">
+            Confirm
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -114,8 +178,7 @@ export default {
     showAdditionError: false,
     heroCreation: {realName: "", publicName: ""},
     heroToDelete: {},
-    heroesToAdd: [],
-    selectedHero: {},
+    heroesToAdd: []
   }),
   computed: {
     ...mapGetters(["getHeroes", "getCurrentHero", "getCurrentTeam", "getCurrentOrganisation"]),
@@ -125,53 +188,53 @@ export default {
         headers.push({text: 'Actions', value: 'actions', sortable: false})
       return headers
     },
+    selected: {
+      get() {
+        return this.getCurrentHero;
+      },
+      set(selectedHeroes) {
+        this.setCurrentHero(selectedHeroes[0]);
+      },
+    },
     addableHero() {
-      return this.getHeroes.filter(hero => !this.getCurrentTeam.members.some(member => member._id === hero._id));
+      return this.getHeroes
     },
     members() {
-      return this.getCurrentTeam.members || [];
-    },
-    select: {
-      get() {
-        return this.getCurrentHero || {};
-      },
-      set(selectedHero) {
-        this.setCurrentHero(selectedHero);
-      },
-    },
+      return this.getCurrentTeam.members
+    }
   },
   methods: {
-    ...mapActions(["getHeroesData", "setCurrentHero", "createHero", "getOrganisationById", "addTeamToOrganisation", "removeHeroFromTeam", "addHeroToTeam", "setCurrentTeam"]),
+    ...mapActions(["getHeroesData", "setCurrentHero", "createHero", "getOrganisationById", "addTeamToOrganisation", "removeHeroFromTeam", "addHeroToTeam", "addHeroToTeam"]),
     selectHero() {
       // TODO Add popup
     },
     async confirmCreate() {
       const answer = await this.createHero(this.heroCreation);
       if (answer.error === 0) {
-        this.heroCreation = { realName: "", publicName: "" };
+        this.heroCreation = {realName: "", publicName: ""};
+        // await this.getTeamsData();
         this.showCreateDialog = false;
         this.showCreationError = false;
 
+        // If the component is used to display an organisation's teams
         if (this.heroList) {
-          const updatedTeam = await this.addHeroToTeam(answer.data._id);
-          this.setCurrentTeam(updatedTeam);
+          await this.addHeroToTeam(answer.data._id)
+          // await this.getOrganisationById()
         }
       } else {
         console.log(answer);
         this.showCreationError = true;
       }
     },
-    deletionAction(hero) {
-      this.heroToDelete = hero;
-      this.showDeletionDialog = true;
+    delectionAction(hero) {
+      this.heroToDelete = hero
+      this.showDeletionDialog = true
     },
     async confirmDeletion() {
-      console.log(this.heroToDelete);
-      const answer = await this.removeHeroFromTeam(this.heroToDelete._id)
+      const answer = await this.removeHeroFromTeam(this.heroToDelete)
       if (answer.error === 0) {
         this.heroToDelete = {}
-        const updatedTeam = await this.getOrganisationById()
-        this.setCurrentTeam(updatedTeam);
+        // await this.getOrganisationById()
         this.showDeletionDialog = false
         this.showDeletionError = false
       } else {
@@ -179,18 +242,13 @@ export default {
         this.showDeletionError = true
       }
     },
+    // TODO
     async confirmAddition() {
       let anErrorOccurred = false
       let answer = null
-
       answer = await this.addHeroToTeam(this.heroesToAdd.map(hero => hero._id))
       if (answer.error !== 0) anErrorOccurred = true
-
-      if (!anErrorOccurred) {
-        const updatedTeam = await this.getOrganisationById();
-        this.setCurrentTeam(updatedTeam.team);
-      }
-
+      await this.getOrganisationById()
       if (anErrorOccurred)
         this.showAdditionError = true
       else {
@@ -203,5 +261,6 @@ export default {
   async mounted() {
     await this.getHeroesData();
   },
+
 };
 </script>
--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,194 +1,194 @@
 import AppService from '../../services/app.service';
 
 export default {
-    namespace: true,
-    state: () => ({
-        organisationsPassword: null,
-        organisations: [],
-        currentOrganisation: null,
-        teams: [],
-        currentTeam: null,
-        heroes: [],
-        currentHero: null
-    }),
-    getters: {
-        getOrganisationsPassword: state => state.organisationsPassword,
-        getOrganisations: state => state.organisations,
-        getCurrentOrganisation: state => state.currentOrganisation,
-        getTeams: state => state.teams,
-        getCurrentTeam: state => state.currentTeam,
-        getHeroes: state => state.heroes,
-        getCurrentHero: state => state.currentHero,
-        isAuthenticated: state => !!state.organisationsPassword && !!state.currentOrganisation
+  namespace: true,
+  state: () => ({
+    organisationsPassword: null,
+    organisations: [],
+    currentOrganisation: null,
+    teams: [],
+    currentTeam: null,
+    heroes: [],
+    currentHero: null
+  }),
+  getters: {
+    getOrganisationsPassword: state => state.organisationsPassword,
+    getOrganisations: state => state.organisations,
+    getCurrentOrganisation: state => state.currentOrganisation,
+    getTeams: state => state.teams,
+    getCurrentTeam: state => state.currentTeam,
+    getHeroes: state => state.heroes,
+    getCurrentHero: state => state.currentHero,
+    isAuthenticated: state => !!state.organisationsPassword && !!state.currentOrganisation
+  },
+  mutations: {
+    updateOrganisationsPassword(state, password) {
+      state.organisationsPassword = password
     },
-    mutations: {
-        updateOrganisationsPassword(state, password) {
-            state.organisationsPassword = password
-        },
-        updateOrganisationsNames(state, names) {
-            state.organisations = names
-        },
-        updateCurrentOrganisation(state, organisation) {
-            state.currentOrganisation = organisation
-        },
-        updateTeams(state, teams) {
-            state.teams = teams
-        },
-        updateCurrentTeam(state, team) {
-            state.currentTeam = team
-        },
-        addHeroTeam(state, data) {
-            state.teams = state.teams.map(team => {
-                if (team.id === data.id) {
-                    return data
-                }
-                return team
-            })
-        },
-        removeHeroTeam(state, data) {
-            state.teams = state.teams.map(team => {
-                if (team.id === data.id) {
-                    return data
-                }
-                return team
-            })
-        },
-        removeHeroCurrentTeam(state, data) {
-            state.currentTeam.members = state.currentTeam.members.filter(member => member._id !== data._id)
-        },
-        updateHeroes(state, data) {
-            state.heroes = data
-        },
-        updateCurrentHero(state, data) {
-            state.currentHero = data
-        },
-        addMemberToCurrentTeam(state, data) {
-            state.currentTeam.members.push(data)
-        },
+    updateOrganisationsNames(state, names) {
+      state.organisations = names
     },
-    actions: {
-        async getOrganisationsData({commit}) {
-            const organisations = await AppService.getOrganisations()
-
-            if (organisations.error === 0) {
-                commit('updateOrganisationsNames', organisations.data)
-            } else
-                console.log(organisations.data)
-
-            return organisations
-        },
-        async getOrganisationById({commit}) {
-            if (!this.getters.getCurrentOrganisation) return
-            const organisation = await AppService.getOrganisationById(this.getters.getCurrentOrganisation._id, this.getters.getOrganisationsPassword)
-            if (organisation.error === 0) {
-                commit('updateCurrentOrganisation', organisation.data[0])
-            } else
-                console.log(organisation.data)
-
-            return organisation
-        },
-        async addTeamToOrganisation(context, idTeam) {
-            const organisation = await AppService.addTeamToOrganisation(idTeam, this.getters.getOrganisationsPassword)
-            if (organisation.error !== 0) {
-                console.log(organisation.data)
-            }
-            return organisation
-        },
-        async removeTeamFromOrganisation(context, idTeam) {
-            const organisation = await AppService.removeTeamFromOrganisation(idTeam, this.getters.getOrganisationsPassword)
-            if (organisation.error !== 0) {
-                console.log(organisation.data)
-            }
-            return organisation
-        },
-        async createOrganisation({commit}, organisation) {
-            const newOrganisation = await AppService.createOrganisation(organisation)
-            if (newOrganisation.error === 0) {
-                commit('updateOrganisationsNames', newOrganisation.data)
-            } else
-                console.log(newOrganisation.data)
-            return newOrganisation
-        },
-        async setOrganisationsPassword({commit}, password) {
-            commit('updateOrganisationsPassword', password)
-        },
-        async getTeamsData({commit}) {
-            const teams = await AppService.getTeams()
-
-            if (teams.error === 0) {
-                commit('updateTeams', teams.data)
-            } else
-                console.log(teams.data)
-
-            return teams
-        },
-        async setCurrentTeam({commit}, data) {
-            if (!data) {
-              commit('updateCurrentTeam', null)
-              return
-            }
-            console.log(data)
-            const teamMembers = await Promise.all(data.members.map(async memberId => {
-              const res = await AppService.getHeroById(memberId, this.getters.getOrganisationsPassword)
-              if (res.error === 0)
-                return res.data[0]
-              else
-                console.log(res)
-            }));
-            console.log('teamMembers:', teamMembers);
-            data.members = teamMembers.filter(Boolean);
-            commit('updateCurrentTeam', data)
-          },
-        async createTeam({commit}, team) {
-            const answer = await AppService.createTeam(team)
-            if (answer.error === 0) {
-                commit('updateTeams', [...this.getters.getTeams, answer.data])
-            } else
-                console.log(answer.data)
-            return answer
-        },
-        async addHeroToTeam({commit}, data) {
-            const answer = await AppService.addHeroToTeam(this.getters.getCurrentTeam._id, data)
-            if (answer.error === 0) {
-                commit('addHeroTeam', answer.data)
-                commit('addMemberToCurrentTeam', data);
-            } else
-                console.log(answer.data)
-            return answer
-        },
-        async removeHeroFromTeam({commit}, data) {
-            console.log('data ',data)
-            const answer = await AppService.removeHeroFromTeam(this.getters.getCurrentTeam._id, data)
-            if (answer.error === 0) {
-                commit('removeHeroTeam', answer.data)
-                commit('removeHeroCurrentTeam', data)
-            } else
-                console.log(answer.data)
-            return answer
-        },
-        async getTeamById({commit}){
-            if (!this.getters.getCurrentTeam) return
-            const teamMembers = await AppService.getTeamMembers(this.getters.getCurrentTeam._id)
-            if (teamMembers.error === 0) {
-                commit('updateCurrentOrganisation', teamMembers.data[0])
-            } else
-                console.log(teamMembers.data)
-
-            return teamMembers
-        },
-        async createHero(context, hero) {
-            const answer = await AppService.createHero(hero)
-            return answer
-        },
-        async getHeroesData({commit}) {
-            const answer = await AppService.getHeroes()
-            if (answer.error === 0)
-                commit('updateHeroes', answer.data)
-            else
-                console.log(answer)
-        },
-         async setCurrentHero({commit}, data){
-            commit('updateCurrentHero', data)
-         }
+    updateCurrentOrganisation(state, organisation) {
+      state.currentOrganisation = organisation
     },
+    updateTeams(state, teams) {
+      state.teams = teams
+    },
+    updateCurrentTeam(state, team) {
+      state.currentTeam = team
+    },
+    addHeroTeam(state, data) {
+      state.teams = state.teams.map(team => {
+        if (team.id === data.id) {
+          return data
+        }
+        return team
+      })
+    },
+    removeHeroTeam(state, data) {
+      state.teams = state.teams.map(team => {
+        if (team.id === data.id) {
+          return data
+        }
+        return team
+      })
+    },
+    removeHeroCurrentTeam(state, data) {
+      state.currentTeam.members = state.currentTeam.members.filter(member => member._id !== data._id)
+    },
+    updateHeroes(state, data) {
+      state.heroes = data
+    },
+    updateCurrentHero(state, data) {
+      state.currentHero = data
+    },
+    addMemberToCurrentTeam(state, data) {
+      state.currentTeam.members.push(data)
+    },
+  },
+  actions: {
+    async getOrganisationsData({commit}) {
+      const organisations = await AppService.getOrganisations()
+
+      if (organisations.error === 0) {
+        commit('updateOrganisationsNames', organisations.data)
+      } else
+        console.log(organisations.data)
+
+      return organisations
+    },
+    async getOrganisationById({commit}) {
+      if (!this.getters.getCurrentOrganisation) return
+      const organisation = await AppService.getOrganisationById(this.getters.getCurrentOrganisation._id, this.getters.getOrganisationsPassword)
+      if (organisation.error === 0) {
+        commit('updateCurrentOrganisation', organisation.data[0])
+      } else
+        console.log(organisation.data)
+
+      return organisation
+    },
+    async addTeamToOrganisation(context, idTeam) {
+      const organisation = await AppService.addTeamToOrganisation(idTeam, this.getters.getOrganisationsPassword)
+      if (organisation.error !== 0) {
+        console.log(organisation.data)
+      }
+      return organisation
+    },
+    async removeTeamFromOrganisation(context, idTeam) {
+      const organisation = await AppService.removeTeamFromOrganisation(idTeam, this.getters.getOrganisationsPassword)
+      if (organisation.error !== 0) {
+        console.log(organisation.data)
+      }
+      return organisation
+    },
+    async createOrganisation({commit}, organisation) {
+      const newOrganisation = await AppService.createOrganisation(organisation)
+      if (newOrganisation.error === 0) {
+        commit('updateOrganisationsNames', newOrganisation.data)
+      } else
+        console.log(newOrganisation.data)
+      return newOrganisation
+    },
+    async setOrganisationsPassword({commit}, password) {
+      commit('updateOrganisationsPassword', password)
+    },
+    async getTeamsData({commit}) {
+      const teams = await AppService.getTeams()
+
+      if (teams.error === 0) {
+        commit('updateTeams', teams.data)
+      } else
+        console.log(teams.data)
+
+      return teams
+    },
+    async setCurrentTeam({commit}, data) {
+      if (!data) {
+        commit('updateCurrentTeam', null)
+        return
+      }
+      console.log(data)
+      const teamMembers = await Promise.all(data.members.map(async memberId => {
+        const res = await AppService.getHeroById(memberId, this.getters.getOrganisationsPassword)
+        if (res.error === 0)
+          return res.data[0]
+        else
+          console.log(res)
+      }));
+      console.log('teamMembers:', teamMembers);
+      data.members = teamMembers.filter(Boolean);
+      commit('updateCurrentTeam', data)
+    },
+    async createTeam({commit}, team) {
+      const answer = await AppService.createTeam(team)
+      if (answer.error === 0) {
+        commit('updateTeams', [...this.getters.getTeams, answer.data])
+      } else
+        console.log(answer.data)
+      return answer
+    },
+    async addHeroToTeam({commit}, data) {
+      const answer = await AppService.addHeroToTeam(this.getters.getCurrentTeam._id, data)
+      if (answer.error === 0) {
+        commit('addHeroTeam', answer.data)
+        commit('addMemberToCurrentTeam', data);
+      } else
+        console.log(answer.data)
+      return answer
+    },
+    async removeHeroFromTeam({commit}, data) {
+      console.log('data ',data)
+      const answer = await AppService.removeHeroFromTeam(this.getters.getCurrentTeam._id, data)
+      if (answer.error === 0) {
+        commit('removeHeroTeam', answer.data)
+        commit('removeHeroCurrentTeam', data)
+      } else
+        console.log(answer.data)
+      return answer
+    },
+    async getTeamById({commit}){
+      if (!this.getters.getCurrentTeam) return
+      const teamMembers = await AppService.getTeamMembers(this.getters.getCurrentTeam._id)
+      if (teamMembers.error === 0) {
+        commit('updateCurrentOrganisation', teamMembers.data[0])
+      } else
+        console.log(teamMembers.data)
+
+      return teamMembers
+    },
+    async createHero(context, hero) {
+      const answer = await AppService.createHero(hero)
+      return answer
+    },
+    async getHeroesData({commit}) {
+      const answer = await AppService.getHeroes()
+      if (answer.error === 0)
+        commit('updateHeroes', answer.data)
+      else
+        console.log(answer)
+    },
+    async setCurrentHero({commit}, data){
+      commit('updateCurrentHero', data)
+    }
+  },
 }
